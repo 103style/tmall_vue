faa820ecb410ddab5fa53d155c9d98073e93e6f6
103style <wtimexiaoke@gmail.com>
2021-01-16 14:05:38 +0800

    finish nav_tabs

--- src/app.vue
+++ src/app.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <NavTopSite></NavTopSite>
     <Header></Header>
+    <NavTabs></NavTabs>
   </div>
 </template>
 
 <script>
 import NavTopSite from "./tmail/nav_top_site.vue";
 import Header from "./tmail/header.vue"
+import NavTabs from "./tmail/nav_tabs.vue"
 export default {
   components: {
     NavTopSite,
     Header,
+    NavTabs,
   },
 };
 </script>
